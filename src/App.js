baffdbd1ec4b3b261d745c02f39f7ba5a810a05f
Mpino19 <mykmm19@gmail.com>
2024-06-24 14:50:35 +0900

    JSONplaceholderからユーザー情報を取得

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => setUsers(data));
+  }, []);
+
   return (
     <div className="App">
       <div className="main">
         <h2>検索アプリ</h2>
         <input type="text" />
         <div className="content">
-          <div className="box">
-            <h3>ユーザー名</h3>
-            <hr />
-            <p>メールアドレス</p>
-          </div>
+          {users.map((user) => (
+            <div className="box">
+              <h3>{user.name}</h3>
+              <hr />
+              <p>{user.email}</p>
+            </div>
+          ))}
           <div className="box">
             <h3>ユーザー名</h3>
             <hr />
